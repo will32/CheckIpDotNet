27647e6517386e8143d4affc7f39e15a41bc4708
William Lin <linzhijun032@gmail.com>
2019-04-10 01:40:43 -0700

    init

---
+++ CheckIpDotNet/Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CheckIpDotNet.Controllers
+{
+    [Route("/")]
+    [ApiController]
+    public class HomeController : Controller
+    {
+        private IHttpContextAccessor _accessor;
+        public HomeController(IHttpContextAccessor accessor) : base()
+        {
+            _accessor = accessor;
+        }
+
+        [HttpGet]
+        public string Index()
+        {
+            return _accessor.HttpContext.Connection.RemoteIpAddress.ToString();
+        }
+    }
+}
